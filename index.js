5cc701376142566f1a749c26f8168dca17d74313
+mitchellengetich <+mitchellngetich24@gmail.com>
2025-03-20 18:53:20 +0300

    -looping-code-along

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function writeCards(names, event = "birthday") {
+    let occasion = event;
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${occasion} gift!`);
+    }
+    return messages;
+}
+
+console.log(writeCards(["Guadalupe", "Ollie", "Aki"], "surprise"));
+
+function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+        console.log(i);
+    }
+}
+
+countDown(10);
+
